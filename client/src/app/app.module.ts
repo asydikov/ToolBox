c5565e620d39cce346ac364be81c9dd7cc87df87
Aziz Sydikov <asydikov@gmail.com>
2020-02-29 21:09:30 +0000

    Error interceptor added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NavComponent } from './nav/nav.component';
 import { JwtInterceptor } from './_helpers/jwt-interceptor';
+import { ErrorInterceptor } from './_helpers/error-interceptor';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { JwtInterceptor } from './_helpers/jwt-interceptor';
     AppRoutingModule    
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
   bootstrap: [AppComponent]
 })
