6e827f02f6f272902277eeb7f02d5a8b72bd8392
Aziz Sydikov <asydikov@gmail.com>
2020-03-06 17:30:14 +0000

    Server badge component added

---
+++ client/src/app/sql-monitor/server/server-badge/server-badge.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-badge',
+  templateUrl: './server-badge.component.html',
+  styleUrls: ['./server-badge.component.scss']
+})
+export class ServerBadgeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
