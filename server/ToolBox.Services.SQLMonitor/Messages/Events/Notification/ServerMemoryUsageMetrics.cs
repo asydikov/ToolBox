db4d8c8777a6d094b75a9cd4ed588c5c1d2eca09
Aziz Sydikov <asydikov@gmail.com>
2020-03-07 00:05:10 +0000

    Create ServerMemoryUsageMetrics.cs

---
+++ server/ToolBox.Services.SQLMonitor/Messages/Events/Notification/ServerMemoryUsageMetrics.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+using ToolBox.Common.Events;
+using Toolbox.Common.Messages;
+
+namespace ToolBox.Services.SQLMonitor.Messages.Events.Notification
+{
+    [MessageNamespace("notification")]
+    public class ServerMemoryUsageMetrics : IEvent
+    {
+        public Guid Id { get; }
+        public Guid UserId { get; }
+        public Guid ServerId { get; set; }
+        public int RequestsCount { get; }
+        public int PageReadsCount { get; }
+        public int PageLifetime { get; }
+
+
+        [JsonConstructor]
+        public ServerMemoryUsageMetrics(
+            Guid id, Guid userId, Guid serverId, int requestsCount, int pageReadsCount, int pageLifetime)
+        {
+            Id = id;
+            UserId = userId;
+            ServerId = serverId;
+            RequestsCount = requestsCount;
+            PageReadsCount = pageReadsCount;
+            PageLifetime = pageLifetime;
+        }
+    }
+}
