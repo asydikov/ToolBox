637001950ac3bc00c24e07bb70ac07e4d966e091
Aziz Sydikov <asydikov@gmail.com>
2020-04-13 20:56:31 +0100

    SqlQueryNames refactored

--- src/ToolBox.Services.SQLMonitor/Domain/Enums/SqlQueryNames.cs
+++ src/ToolBox.Services.SQLMonitor/Domain/Enums/SqlQueryNames.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ToolBox.Services.SQLMonitor.Domain.Enums
+﻿namespace ToolBox.Services.SQLMonitor.Domain.Enums
 {
     public enum SqlQueryNames
     {
