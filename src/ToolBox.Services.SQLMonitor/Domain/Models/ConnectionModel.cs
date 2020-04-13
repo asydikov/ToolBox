96f138e00873a6f9d5dd0295618aff414655974f
Aziz Sydikov <asydikov@gmail.com>
2020-04-13 20:56:56 +0100

    SQLMonitor ConnectionMOdel clas refactored

--- src/ToolBox.Services.SQLMonitor/Domain/Models/ConnectionModel.cs
+++ src/ToolBox.Services.SQLMonitor/Domain/Models/ConnectionModel.cs
@@ -1,9 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace ToolBox.Services.SQLMonitor.Domain.Models
 {
     public class ConnectionModel
