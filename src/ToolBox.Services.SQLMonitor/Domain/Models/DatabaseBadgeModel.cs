26ea6fbd2ca64749d468f77959cb00c18aac9748
Aziz Sydikov <asydikov@gmail.com>
2020-04-14 22:34:28 +0100

    DatabaseBadgeModel deleted

---
+++ src/ToolBox.Services.SQLMonitor/Domain/Models/DatabaseBadgeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ToolBox.Services.SQLMonitor.Domain.Models
+{
+    public class DatabaseBadgeModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? FullBackupTime { get; set; }
+        public DateTime? DifferentialBackupTime { get; set; }
+        public DateTime? TransactionLogBackupTime { get; set; }
+        public string RecoveryModel { get; set; }
+        public double Space { get; set; }
+        public double UnallocatedSpace { get; set; }
+        public string Unit { get; set; }
+    }
+}
