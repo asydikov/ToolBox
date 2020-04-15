6b2d1c330245e0e16fed171be062fe515eab1065
Aziz Sydikov <asydikov@gmail.com>
2020-04-15 22:20:25 +0100

    SqlMonitor/EfExtension class modifies

--- src/ToolBox.Services.SQLMonitor/EF/EfExtension.cs
+++ src/ToolBox.Services.SQLMonitor/EF/EfExtension.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ToolBox.Services.SQLMonitor.EF
 {
