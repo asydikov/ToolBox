9e5f8de03f733fb709e761bbc401c5ae5c2a248e
Aziz Sydikov <asydikov@gmail.com>
2020-04-15 22:19:36 +0100

    SqlServerBadgeModel removed

---
+++ src/ToolBox.Services.SQLMonitor/Domain/Models/SqlServerBadgeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToolBox.Services.SQLMonitor.Domain.Models
+{
+    public class SqlServerBadgeModel
+    {
+        public Guid ServerId { get; set; }
+        public string Name { get; set; }
+        public string ServerAddress { get; set; }
+        public string Description { get; set; }
+        public double PageReadsCount { get; set; }
+        public double RequestCount { get; set; }
+        public double PageLifetime { get; set; }
+        public bool IsAlive { get; set; }
+        public int ConnectedUsers { get; set; }
+    }
+}
