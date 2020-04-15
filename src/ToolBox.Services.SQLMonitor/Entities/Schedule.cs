0afc6d934418834c9304a2c7e2c32ee24351f142
Aziz Sydikov <asydikov@gmail.com>
2020-04-15 22:22:03 +0100

    Schedule class refactored

--- src/ToolBox.Services.SQLMonitor/Entities/Schedule.cs
+++ src/ToolBox.Services.SQLMonitor/Entities/Schedule.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ToolBox.Services.SQLMonitor.Entities;
+
 
 namespace ToolBox.Services.SQLMonitor.Entities
 {
