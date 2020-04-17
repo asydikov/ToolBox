5396c6f315209e2e67447cc505afa270020b1d6a
Aziz Sydikov <asydikov@gmail.com>
2020-04-17 23:28:56 +0100

    Update SqlStatementQuery.cs

--- src/ToolBox.Services.SQLMonitor/Messages/Commands/DbWorker/SqlStatementQuery.cs
+++ src/ToolBox.Services.SQLMonitor/Messages/Commands/DbWorker/SqlStatementQuery.cs
@@ -1,11 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Toolbox.Common.Messages;
 using ToolBox.Common.Commands;
-using ToolBox.Services.SQLMonitor.Domain.Enums;
 
 namespace ToolBox.Services.SQLMonitor.Messages.Commands.DbWorker
 {
