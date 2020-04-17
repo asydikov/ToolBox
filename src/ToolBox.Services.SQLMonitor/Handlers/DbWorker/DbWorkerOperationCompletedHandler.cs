bc58bc323df53f88eebe92fa9aacde47be82bce9
Aziz Sydikov <asydikov@gmail.com>
2020-04-17 23:27:59 +0100

    Update DbWorkerOperationCompletedHandler.cs

--- src/ToolBox.Services.SQLMonitor/Handlers/DbWorker/DbWorkerOperationCompletedHandler.cs
+++ src/ToolBox.Services.SQLMonitor/Handlers/DbWorker/DbWorkerOperationCompletedHandler.cs
@@ -1,19 +1,13 @@
 ﻿using Microsoft.Extensions.Logging;
 using RawRabbit;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using ToolBox.Common.Events;
 using ToolBox.Services.SQLMonitor.Domain.Enums;
 using ToolBox.Services.SQLMonitor.Messages.Events.DbWorker;
-using ToolBox.Services.SQLMonitor.Messages.Events.Notification;
 using ToolBox.Services.SQLMonitor.Services;
 
 namespace ToolBox.Services.SQLMonitor.Handlers.DbWorker
 {
-
-  //  201 Compass House -> wednesday  12:50
     public class DbWorkerOperationCompletedHandler : IEventHandler<DbWorkerOperationCompleted>
     {
         private readonly IBusClient _busClient;
