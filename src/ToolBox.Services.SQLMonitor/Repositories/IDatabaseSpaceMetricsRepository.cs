487654c31ded1e6bf102dfb518f4a944cbabf1bb
Aziz Sydikov <asydikov@gmail.com>
2020-04-18 23:10:36 +0100

    Update IDatabaseSpaceMetricsRepository.cs

--- src/ToolBox.Services.SQLMonitor/Repositories/IDatabaseSpaceMetricsRepository.cs
+++ src/ToolBox.Services.SQLMonitor/Repositories/IDatabaseSpaceMetricsRepository.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ToolBox.Services.SQLMonitor.Entities;
 
 namespace ToolBox.Services.SQLMonitor.Repositories
 {
-   public interface IDatabaseSpaceMetricsRepository
+   public interface IDatabaseSpaceMetricsRepository:IRepositoryBase<DatabaseSpaceMetrics>
     {
     }
 }
