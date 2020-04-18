599402524dac5cd49ff315a091cd8988ed638769
Aziz Sydikov <asydikov@gmail.com>
2020-04-18 23:09:00 +0100

    Update IDatabaseBackupMetricsRepository.cs

--- src/ToolBox.Services.SQLMonitor/Repositories/IDatabaseBackupMetricsRepository.cs
+++ src/ToolBox.Services.SQLMonitor/Repositories/IDatabaseBackupMetricsRepository.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+
+using ToolBox.Services.SQLMonitor.Entities;
 
 namespace ToolBox.Services.SQLMonitor.Repositories
 {
-   public interface IDatabaseBackupMetricsRepository
+   public interface IDatabaseBackupMetricsRepository : IRepositoryBase<DatabaseBackupMetrics>
     {
     }
 }
