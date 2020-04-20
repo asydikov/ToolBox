b4896c528f041c3077a997ecb21309d203f099c4
Aziz Sydikov <asydikov@gmail.com>
2020-04-20 23:20:16 +0100

    Update ScheduleRepository.cs

--- src/ToolBox.Services.SQLMonitor/Repositories/ScheduleRepository.cs
+++ src/ToolBox.Services.SQLMonitor/Repositories/ScheduleRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ToolBox.Services.SQLMonitor.EF;
+﻿using ToolBox.Services.SQLMonitor.EF;
 using ToolBox.Services.SQLMonitor.Entities;
 
 namespace ToolBox.Services.SQLMonitor.Repositories
