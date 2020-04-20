a7bffbc366158d41df8c5c5c9f7032214775ab1c
Aziz Sydikov <asydikov@gmail.com>
2020-04-20 23:20:14 +0100

    Update ISqlQueryRepository.cs

--- src/ToolBox.Services.SQLMonitor/Repositories/ISqlQueryRepository.cs
+++ src/ToolBox.Services.SQLMonitor/Repositories/ISqlQueryRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ToolBox.Services.SQLMonitor.Entities;
+﻿using ToolBox.Services.SQLMonitor.Entities;
 
 namespace ToolBox.Services.SQLMonitor.Repositories
 {
-    interface ISqlQueryRepository
+    interface ISqlQueryRepository:IRepositoryBase<SqlQuery>
     {
     }
 }
