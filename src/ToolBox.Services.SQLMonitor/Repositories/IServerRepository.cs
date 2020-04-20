98eb028a2c513fcb23f91a07138d426e0e10de76
Aziz Sydikov <asydikov@gmail.com>
2020-04-20 23:20:12 +0100

    Update IServerRepository.cs

--- src/ToolBox.Services.SQLMonitor/Repositories/IServerRepository.cs
+++ src/ToolBox.Services.SQLMonitor/Repositories/IServerRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ToolBox.Services.SQLMonitor.Entities;
+﻿using ToolBox.Services.SQLMonitor.Entities;
 
 namespace ToolBox.Services.SQLMonitor.Repositories
 {
-    public interface IServerRepository
+    public interface IServerRepository:IRepositoryBase<Server>
     {
     }
 }
