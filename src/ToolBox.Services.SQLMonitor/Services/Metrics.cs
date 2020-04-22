47d3f397f7ffa4a6ac568780c7759b166623971c
Aziz Sydikov <asydikov@gmail.com>
2020-04-22 22:05:35 +0100

    Update Metrics.cs

--- src/ToolBox.Services.SQLMonitor/Services/Metrics.cs
+++ src/ToolBox.Services.SQLMonitor/Services/Metrics.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using RawRabbit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.DataProtection;
 using ToolBox.Services.SQLMonitor.Domain.Enums;
