79dbcf3fd6854ff843606d91da1c3ccb7176386a
Aziz Sydikov <asydikov@gmail.com>
2020-04-22 22:05:25 +0100

    Update IDatabaseBackupMetricsService.cs

--- src/ToolBox.Services.SQLMonitor/Services/IDatabaseBackupMetricsService.cs
+++ src/ToolBox.Services.SQLMonitor/Services/IDatabaseBackupMetricsService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ToolBox.Services.SQLMonitor.Domain.Models;
+﻿using ToolBox.Services.SQLMonitor.Domain.Models;
 using ToolBox.Services.SQLMonitor.Entities;
 
 namespace ToolBox.Services.SQLMonitor.Services
